20985c3e760ed85f44363ecd69c58e74d0e82e84
Daniel Childs <dchilds@cnitucson.com>
2014-10-30 16:33:13 -0700

    Added contact information and correct google maps api key

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,11 +1,26 @@
-function createMap(containerid, parameters) {
-  var mymap = document.getElementById(containerid),
-  map_options = {
-    zoom: 13,
-    scrollwheel: false,
-    /* rest of options */
-  },
-  map = new google.maps.Map(mymap, map_options);
-
-  /* 'rest of code' to take parameters into account */
+//Google Maps
+// Enable the visual refresh
+function initialize() {
+	var myLatlng = new google.maps.LatLng(32.252047, -111.005131)
+	google.maps.visualRefresh = true;
+	var mapOptions = {
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+//		zoom: 14,
+		zoom: 12,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+	
+	var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map
+  });
+}
+if (document.getElementById("map-canvas")) {
+	google.maps.event.addDomListener(window, 'load', initialize);
 }
